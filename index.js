51f0a1780c675b60c613a217d11addff6e35f174
tcsx <username@users.noreply.github.com>
2018-04-08 23:51:48 -0700

    change listening port

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const domains = require('./src/players/players');
 const { URL } = require('url');
-const path = require('path');
 
+const PORT = process.env.PORT;
 for (const player in domains) {
     const { port, hostname } = new URL(domains[player]);
     const app = express();
 
     require('./routes')(app);
 
-    app.listen(port, hostname);
+    app.listen(port || PORT, hostname);
 }
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    const app = express();
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
 
-const app = express();
-app.use(express.static('build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000);
+    app.listen(PORT);
+}
